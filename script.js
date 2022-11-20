128771950dad821c8e13279d280ffd614d95ccfe
Eduardo Reis <116650399+reisseduardo@users.noreply.github.com>
2022-11-20 19:23:39 -0300

    criando e testando const

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 const botao = document.querySelector('.btn');
+const dataUsuario = new Date(document.querySelector('input[type="date"]'));
+const date = new Date;
+const today = date.getDate();
+const month = date.getMonth() + 1;
+const year = date.getFullYear();
+const todayDate = today + "/" + month + "/" + year;
+
+const second = 1000;
+const minute = second * 60;
+const hour = minute * 60;
+const day = hour * 24;
+
+
+
+console.log(date);
