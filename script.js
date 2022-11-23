c1ab255a5dbc608ad4a0adff328274efca2cda35
Eduardo Reis <116650399+reisseduardo@users.noreply.github.com>
2022-11-23 12:43:41 -0300

    limpando o código

--- script.js
+++ script.js
@@ -2,49 +2,47 @@ const second = 1000;
 const minute = second * 60;
 const hour = minute * 60;
 const day = hour * 24;
+const modal = document.querySelector('.modal');
 
 function countDays() {
-    const hoje = new Date()
-    const dia = hoje.getDate().toString().padStart(2, '0')
-    const mes = String(hoje.getMonth() + 1).padStart(2, '0')
-    const ano = hoje.getFullYear()
-    const dataAtual = `${dia} / ${mes} / ${ano}`
-    let date_end = new Date(document.form_main.date_end.value);
+ 	const hoje = new Date()
+	const dia = hoje.getDate().toString().padStart(2,'0')
+	const mes = String(hoje.getMonth() + 1).padStart(2,'0')
+	const ano = hoje.getFullYear()	
+ 	let dataUsuario = new Date(document.form_main.dataUsuario.value);
 
-    let diff = date_end.getTime() - hoje.getTime();
+ 	let diff = dataUsuario.getTime() - hoje.getTime();
 
-    document.getElementById('days').innerText = Math.floor(diff / day);
+ 	document.getElementById('days').innerText = Math.floor(diff / day);
 }
 
 const trocaModal = () => {
-    const modal = document.querySelector('.modal');
-    const estiloAtual = modal.style.display;
-    event.preventDefault();
-    countDays();
-    if (estiloAtual == 'block') {
-        modal.style.display = 'none';
-    } else {
-        modal.style.display = 'block';
-    }
+	const estiloAtual = modal.style.display;
+	event.preventDefault();
+	countDays();
+	if(estiloAtual == 'block'){
+		modal.style.display = 'none';
+	}else{
+		modal.style.display = 'block';
+	}
 }
 const botao = document.querySelector('#btn');
-botao.addEventListener("click", trocaModal)
+	botao.addEventListener("click", trocaModal)
 
-window.onclick = function (event) {
-    const modal = document.querySelector('.modal');
-    if (event.target == modal) {
-        trocaModal();
-    }
+window.onclick = function(event){	
+	if(event.target == modal){
+		trocaModal();
+	}
 }
 const corPadrao = document.querySelector('#corPadrao');
-corPadrao.addEventListener("click", function () {
-    document.body.style.backgroundColor = "lightgrey";
-})
+	corPadrao.addEventListener("click", function(){
+		document.body.style.backgroundColor = "lightgrey";
+	})
 const corAzul = document.querySelector('#corAzul');
-corAzul.addEventListener("click", function () {
-    document.body.style.backgroundColor = "lightblue";
-})
+	corAzul.addEventListener("click", function(){
+		document.body.style.backgroundColor = "lightblue";
+	})
 const corAmarela = document.querySelector('#corAmarela');
-corAmarela.addEventListener("click", function () {
-    document.body.style.backgroundColor = "yellowgreen";
-})
+	corAmarela.addEventListener("click", function(){
+		document.body.style.backgroundColor = "yellowgreen";
+	})
